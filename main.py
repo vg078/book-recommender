f4ae020d926e34d8b018259867eb19267f02a449
Viva Gupta <vivagupta@Vivas-MacBook-Pro.local>
2025-05-01 22:56:00 -0700

    small mod

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
     #user_input = "I am looking for book recommendations for 12 year old girl who is interested in fiction. She loves wings of fire book series. But i would like her to try something new now. Can you please recommend a suitable book for her."
     #prompt = "Summarize the following text in instruction format. Provide summary in one sentence" 
     #summary = my_chatgpt_old.summarize_text(user_input,prompt)
-    print("Search String: ",summary)
+    print("Search String : ",summary)
 
     books_list, books = my_vector_search.search_data_in_csv("books_cleaned.csv","tagged_description",summary)
     print (books[books["isbn13"].isin(books_list)]) 
